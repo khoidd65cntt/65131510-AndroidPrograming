5d352b01f6a086ef2f4c9502e238dd1512766903
Đỗ Đăng Khôi <khoido.280205@gmail.com>
2026-03-27 15:50:51 +0700

    DanhSachTinhThanh

--- BTH7_ListView/app/src/main/java/com/example/listview/MainActivity.java
+++ BTH7_ListView/app/src/main/java/com/example/listview/MainActivity.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ListView lvDanhSach;
-    ArrayList<String> arrPhuongTien;
+    ArrayList<String> arrTinhThanh;
     ArrayAdapter<String> adapter;
 
     @Override
@@ -23,26 +23,26 @@ public class MainActivity extends AppCompatActivity {
 
         lvDanhSach = findViewById(R.id.lvDanhSach);
 
-        arrPhuongTien = new ArrayList<>();
-        arrPhuongTien.add("Xe đạp");
-        arrPhuongTien.add("Xe máy");
-        arrPhuongTien.add("Ô tô");
-        arrPhuongTien.add("Xe tải");
-        arrPhuongTien.add("Máy bay");
-        arrPhuongTien.add("Tàu lửa");
+        arrTinhThanh = new ArrayList<>();
+        arrTinhThanh.add("Hà Nội");
+        arrTinhThanh.add("Thành phố Hồ Chí Minh");
+        arrTinhThanh.add("Đồng Nai");
+        arrTinhThanh.add("Bình Thuận");
+        arrTinhThanh.add("Ninh Thuận");
+        arrTinhThanh.add("Nha Trang");
+        arrTinhThanh.add("Tiền Giang");
 
         adapter = new ArrayAdapter<>(
                 MainActivity.this,
                 android.R.layout.simple_list_item_1,
-                arrPhuongTien
+                arrTinhThanh
         );
 
         lvDanhSach.setAdapter(adapter);
         lvDanhSach.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // position: Vị trí của phần tử được click (bắt đầu từ 0)
-                String monHocĐuocChon = arrPhuongTien.get(position);
+                String monHocĐuocChon = arrTinhThanh.get(position);
 
                 Toast.makeText(MainActivity.this, "Bạn chọn: " + monHocĐuocChon, Toast.LENGTH_SHORT).show();
             }
